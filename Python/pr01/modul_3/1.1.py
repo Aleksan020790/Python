660a1a79dc565b9f993782e6281cb407c473e352
Aleksandra <aleksandrazhadan020790@gmail.com>
2023-04-11 15:25:52 +0300

    Modul_1, modul_3

---
+++ Python/pr01/modul_3/1.1.py
@@ -0,0 +1,5 @@
+a = int (input('input вносимо число a ='))
+b = int (input('input вносимо число b ='))
+c = int (input('input вносимо число c ='))
+res = a*100 + b*10 + c
+print ("res = ", res )
---
+++ Python/pr01/modul_3/1.2.py
@@ -0,0 +1,6 @@
+a =int(input("input вносимо число a="))
+b =int(input("input вносимо число b="))
+c = int (input("input вносимо число c="))
+d = int(input("input вносимо число d="))
+res = a*b*c*d
+print('res =',res)
