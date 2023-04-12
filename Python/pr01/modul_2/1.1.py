5ac8de0efd078489b7539231d5ed12b497f15c74
Aleksandra <aleksandrazhadan020790@gmail.com>
2023-04-12 14:33:35 +0300

    modul_1

--- Python/pr01/modul_2/1.1.py
+++ Python/pr01/modul_2/1.1.py
@@ -1,4 +1,4 @@
-a=int(input('input number:'))
-b=int(input('input number:'))
-c=int(input('input number:'))
+a=int(input('input number :'))
+b=int(input('input number :'))
+c=int(input('input number :'))
 print (a +b + c)
