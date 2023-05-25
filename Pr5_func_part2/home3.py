bd374b82ed3ef35444f800fc6cd06dae8572657c
Aleksandra <aleksandrazhadan020790@gmail.com>
2023-05-25 16:35:29 +0300

    Pr5

--- Pr5_func_part2/home3.py
+++ Pr5_func_part2/home3.py
@@ -1,7 +1,12 @@
 # Напишите функцию, определяющую количество простых чисел в списке целых. Список передаётся в качестве
 # параметра. Полученный результат возвращается из функции.
 
-List = input("Введіть прості цілі числа через пробел: ") 
-List = List.split()
-def count(x):
-    
+
+def count_is_prime(mas):
+    count = 0
+    for i in list1:
+        if count_is_prime(i):
+            count+=1
+    return count
+list1 = [54,765,34,423,654,86,643]
+print(count_is_prime(list1))
--- Pr5_func_part2/home5.py
+++ Pr5_func_part2/home5.py
@@ -2,11 +2,15 @@
 # качестве параметра и возвращает список, содержащий
 # элементы обоих списков.
 
+def contain_both_el(lst1,lst2):
+    for i in range(lst1,lst2):
+        if contain_both_el(i):
+
 
 lst1=[3,2,4,10,22,32]
 lst2=[12,34,32,66,68]
 
-def contain_both_el(lst1,lst2):
+
     
 
 lst3=[lst1+lst2]
